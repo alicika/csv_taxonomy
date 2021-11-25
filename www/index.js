db48e2d01f39f03f6fb36933cb35f0a7bc193da3
alicika <52126032+alicika@users.noreply.github.com>
2021-11-25 23:51:42 +0900

    refactor codes

--- www/index.js
+++ www/index.js
@@ -1,3 +1,17 @@
 import * as wasm from "hello-wasm-pack";
 
 wasm.greet();
+
+$(function() {
+    var options = {
+        target: '#svg_img',
+        url: "/draw",
+        type: "post",
+        success: enableButton
+    };
+    $('#draw').ajaxForm(options);
+});
+
+function enableButton(responseText, statusText, xhr, $form)  {
+    $('#fitplot').prop('disabled', false);
+}
